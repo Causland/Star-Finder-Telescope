d602d35e02a0d89eb53e745f33d9c24d47cee583
Chris Auslander <christopher.auslander@gmail.com>
2024-05-27 14:35:45 -0400

    updates to arduino Motion Controller for horizontal servo with feedback sensor

--- arduino/main/main.ino
+++ arduino/main/main.ino
@@ -2,22 +2,36 @@
 // target positions. Uses sensors to create feedback loop to correct
 // for error in servo motion
 
+#include <algorithm>
+#include <cstring>
+
 #include <Servo.h>
 
 // Constants
-constexpr uint8_t VERT_SERVO_NUM{0};
-constexpr uint8_t VERT_SERVO_PIN{9};
-constexpr uint16_t VERT_SERVO_DEFAULT_US{1500};
+static constexpr uint8_t VERT_SERVO_NUM{0};
+static constexpr uint8_t VERT_SERVO_PIN{9};
+static constexpr uint16_t VERT_SERVO_DEFAULT_US{1500};
+static constexpr uint16_t VERT_SERVO_MIN_US{860};
+static constexpr uint16_t VERT_SERVO_MAX_US{2120};
+static constexpr double VERT_SERVO_MOTION_RANGE_DEG{180};
+static constexpr double VERT_SERVO_US_PER_DEG{(VERT_SERVO_MAX_US - VERT_SERVO_MIN_US) 
+                                                         / VERT_SERVO_MOTION_RANGE_DEG};
 
-constexpr uint8_t HORIZ_SERVO_NUM{1};
-constexpr uint8_t HORIZ_SERVO_PIN{10};
-constexpr uint16_t HORIZ_SERVO_DEFAULT_US{1500};
+static constexpr uint8_t HORIZ_SERVO_NUM{1};
+static constexpr uint8_t HORIZ_SERVO_PIN{10};
+static constexpr uint8_t HORIZ_SERVO_FEEDBACK_PIN{11};
+static constexpr uint16_t HORIZ_SERVO_DEFAULT_US{1500};
+static constexpr uint16_t HORIZ_SERVO_MIN_US{1280};
+static constexpr uint16_t HORIZ_SERVO_MAX_US{1720};
+static constexpr double HORIZ_SERVO_AVG_TEN_US{(HORIZ_SERVO_MAX_TEN_US - HORIZ_SERVO_MIN_TEN_US) / 2.0 + HORIZ_SERVO_MIN_TEN_US};
+static constexpr double HORIZ_SERVO_CW_DEADZONE_TEN_US{148}; 
+static constexpr double HORIZ_SERVO_CCW_DEADZONE_TEN_US{152};
 
-constexpr uint8_t FOCUS_SERVO_NUM{2};
-constexpr uint8_t FOCUS_SERVO_PIN{11};
-constexpr uint16_t FOCUS_SERVO_DEFAULT_US{1500};
+static constexpr uint8_t FOCUS_SERVO_NUM{2};
+static constexpr uint8_t FOCUS_SERVO_PIN{12};
+static constexpr uint16_t FOCUS_SERVO_DEFAULT_US{1500};
 
-constexpr uint8_t COMMAND_LEN{2};
+static constexpr uint8_t COMMAND_LEN{5};
 
 // Globals
 Servo gVertServo;
@@ -25,13 +39,100 @@ Servo gHorizServo;
 Servo gFocusServo;
 byte gCommand[COMMAND_LEN];
 
+int currHorizAngle{0};
+int targetHorizAngle{0};
+int Kp{1}; // Proportionality constant
+
+void measureHorizAngle()
+{
+   // Taken from Paralax 360 Feedback Angle Control code
+   
+   // Constants
+   static constexpr int unitsFC{360};       // Units in a full circle
+   static constexpr int dutyScale{1000};    // Scale duty cycle to 1/1000ths
+   static constexpr int dcMin{29};          // Min duty cycle
+   static constexpr int dcMax{971};         // Max duty cycle
+   static constexpr int q2min{unitsFC / 4}; // For checking if in 1st quadrant
+   static constexpr int q3max{q2min * 3};   // For checking if in 4th quadrant
+
+   static int turns{0};      // For tracking turns
+   static int thetaP{0}; // For tracking angle 
+
+   // Measure low and high times, making sure to take only valid cycle times
+   // (Check for jumps across 0 to 359 degree boundary)
+   int tCycle{0};
+   int tHigh{0};
+   int tLow{0};
+   int dc{0};
+   while(true)
+   {
+      tHigh = pulseIn(HORIZ_SERVO_FEEDBACK_PIN, HIGH);
+      tLow = pulseIn(HORIZ_SERVO_FEEDBACK_PIN, LOW);
+      tCycle = tHigh + tLow;
+      if ((tCycle > 1000) && tCycle < 1200) // Check for valid
+      {
+         break;
+      }
+      dc = (dutyScale * tHigh) / tCycle;
+   }
+
+   // Calculate angle
+   int theta = (unitsFC - 1) - ((dc - dcMin) * unitsFC) / (dcMax - dcMin + 1);
+
+   // Clamp theta to positive
+   theta = std::clamp(theta, 0, unitsFC - 1);
+
+   // If we transition from quadrant 4 to quadrant 1, increase the turn count
+   if ((theta < q2min) && (thetaP > q3max)) ++turns;
+   // If we transition from quadrant 1 to quadrant 4, decrease the turn count
+   if ((thetaP < q2min) && (theta > q3max)) --turns;
+
+   // Construct current angle from turns count and theta
+   if (turns >= 0)
+   {
+      currHorizAngle = (turns * unitsFC) + theta;
+   }
+   else
+   {
+      currHorizAngle = ((turns + 1) * unitsFC) - (unitsFC - theta);
+   }
+
+   thetaP = theta;
+}
+
+void controlHorizServo()
+{
+   int errorAngle{targetHorizAngle - currHorizAngle};
+   int output{errorAngle * Kp};
+   int offset{0};
+
+   output = std::clamp(output, -200, 200);
+   if (errorAngle > 0)
+   {
+      offset = 30;
+   }
+   else if (errorAngle < 0)
+   {
+      offset = -30;
+   }
+   else
+   {
+      offset = 0;
+   }
+
+   gHorizServo.writeMicroseconds(output + offset);
+}
+
 void setup()
 {
    // Attach the servos and move to default positions
    gVertServo.attach(VERT_SERVO_PIN);
    gVertServo.writeMicroseconds(VERT_SERVO_DEFAULT_US);
+   
    gHorizServo.attach(HORIZ_SERVO_PIN);
    gHorizServo.writeMicroseconds(HORIZ_SERVO_DEFAULT_US);
+   pinMode(HORIZ_SERVO_FEEDBACK_PIN, INPUT);
+
    gFocusServo.attach(FOCUS_SERVO_PIN);
    gFocusServo.writeMicroseconds(FOCUS_SERVO_DEFAULT_US);
 
@@ -53,10 +154,22 @@ void loop()
          switch(gCommand[0])
          {
             case VERT_SERVO_NUM:
-               gVertServo.writeMicroseconds(gCommand[1] * 10);
+            {
+               float theta{0.0f};
+               std::memcpy(&theta, gCommand+1, sizeof(float));
+
+               const int numUs{static_cast<int>(std::clamp(theta * VERT_SERVO_US_PER_DEG + VERT_SERVO_MIN_US,
+                                                           static_cast<double>(VERT_SERVO_MIN_US),
+                                                           static_cast<double>(VERT_SERVO_MAX_US)))};
+
+               gVertServo.writeMicroseconds(numUs);
                break;
+            }               
             case HORIZ_SERVO_NUM:
-               gHorizServo.writeMicroseconds(gCommand[1] * 10);
+               float theta{0.0f};
+               std::memcpy(&theta, gCommand+1, sizeof(float));
+
+               targetHorizAngle = theta;
                break;
             case FOCUS_SERVO_NUM:
                gFocusServo.writeMicroseconds(gCommand[1] * 10);
--- src/interfaces/MotionController/AVRMotionController.cpp
+++ src/interfaces/MotionController/AVRMotionController.cpp
@@ -8,18 +8,8 @@
 static constexpr double DEGPERSEC_TO_ROTPERMIN{60.0 / 360.0};
 
 static constexpr uint8_t BASE_VERT_SERVO_NUM{0};
-static constexpr uint8_t BASE_VERT_SERVO_MIN_TEN_US{86};
-static constexpr uint8_t BASE_VERT_SERVO_MAX_TEN_US{212};
-static constexpr double BASE_VERT_SERVO_MOTION_RANGE_DEG{180};
-static constexpr double BASE_VERT_SERVO_TEN_US_PER_DEG{(BASE_VERT_SERVO_MAX_TEN_US - BASE_VERT_SERVO_MIN_TEN_US) 
-                                                         / BASE_VERT_SERVO_MOTION_RANGE_DEG};
 
 static constexpr uint8_t BASE_HORIZ_SERVO_NUM{1};
-static constexpr uint8_t BASE_HORIZ_SERVO_MIN_TEN_US{100};
-static constexpr uint8_t BASE_HORIZ_SERVO_MAX_TEN_US{200};
-static constexpr double BASE_HORIZ_SERVO_AVG_TEN_US{(BASE_HORIZ_SERVO_MAX_TEN_US - BASE_HORIZ_SERVO_MIN_TEN_US) / 2.0 + BASE_HORIZ_SERVO_MIN_TEN_US};
-static constexpr double BASE_HORIZ_SERVO_CW_DEADZONE_TEN_US{144}; 
-static constexpr double BASE_HORIZ_SERVO_CCW_DEADZONE_TEN_US{154};
 
 static constexpr uint8_t FOCUS_SERVO_NUM{2};
 static constexpr uint8_t FOCUS_SERVO_MIN_TEN_US{100};
@@ -27,6 +17,8 @@ static constexpr uint8_t FOCUS_SERVO_MAX_TEN_US{200};
 static constexpr double FOCUS_SERVO_AVG_TEN_US{(FOCUS_SERVO_MAX_TEN_US - FOCUS_SERVO_MIN_TEN_US) / 2.0 + FOCUS_SERVO_MIN_TEN_US};
 static constexpr double FOCUS_SERVO_CW_DEADZONE_TEN_US{144}; 
 static constexpr double FOCUS_SERVO_CCW_DEADZONE_TEN_US{154};
+
+static constexpr uint8_t COMMAND_LEN{5};
 
 void AVRMotionController::moveFocusKnob(const Rotation& rot)
 {
@@ -57,53 +49,40 @@ void AVRMotionController::moveFocusKnob(const Rotation& rot)
                             static_cast<double>(FOCUS_SERVO_MAX_TEN_US));
    }
    
-   // Write to serial device to move focus servo
-   const std::array<uint8_t, 2> command{FOCUS_SERVO_NUM, static_cast<uint8_t>(numTenUs)};
+   // Write to serial device to move focus servo (last 3 bytes unused)
+   const std::array<uint8_t, COMMAND_LEN> command{FOCUS_SERVO_NUM, static_cast<uint8_t>(numTenUs)};
    mySerial.writeToSerial(command.data(), command.size());
 }
 
 void AVRMotionController::moveHorizAngle(const Rotation& rot)
 {
-   // Convert theta_dot to rpm
-   const auto rpm{rot.theta_dot * DEGPERSEC_TO_ROTPERMIN};
-   double numTenUs{BASE_HORIZ_SERVO_AVG_TEN_US}; 
-   
-   if (rpm > 0.0)
-   {
-      // CW rotation
-      // NOLINTNEXTLINE(cppcoreguidelines-avoid-magic-numbers, readability-magic-numbers) : Precalculated coefficients for servo
-      numTenUs = (-0.0082*rpm*rpm) - (0.4462*rpm) + 144.17;
+   // Write to serial device to move horizontal base servo to target position
+   std::array<uint8_t, COMMAND_LEN> command{};
+   command[0] = BASE_HORIZ_SERVO_NUM;
 
-      // Adjust to keep within min and max
-      numTenUs = std::clamp(numTenUs, 
-                            static_cast<double>(BASE_HORIZ_SERVO_MIN_TEN_US), 
-                            static_cast<double>(BASE_HORIZ_SERVO_CW_DEADZONE_TEN_US - 1));
-   }
-   else if (rpm < 0.0)
+   const float theta{static_cast<float>(rot.theta)};
+   const auto* ptr = reinterpret_cast<const uint8_t*>(&theta);
+   for (size_t i = 0; i < sizeof(float); ++i)
    {
-      // CCW rotation
-      // NOLINTNEXTLINE(cppcoreguidelines-avoid-magic-numbers, readability-magic-numbers) : Precalculated coefficients for servo
-      numTenUs = (0.0084*rpm*rpm) - (0.4103*rpm) + 154.06; 
-
-      // Adjust to keep within min and max
-      numTenUs = std::clamp(numTenUs, 
-                            static_cast<double>(BASE_HORIZ_SERVO_CCW_DEADZONE_TEN_US + 1), 
-                            static_cast<double>(BASE_HORIZ_SERVO_MAX_TEN_US));
+      command[i+1] = *(ptr+i);
    }
 
-   // Write to serial device to move base servo
-   const std::array<uint8_t, 2> command{BASE_HORIZ_SERVO_NUM, static_cast<uint8_t>(numTenUs)};
    mySerial.writeToSerial(command.data(), command.size());
 }
 
 void AVRMotionController::moveVertAngle(const Rotation& rot)
 {
-   const auto numTenUs{std::clamp(rot.theta * BASE_VERT_SERVO_TEN_US_PER_DEG + BASE_VERT_SERVO_MIN_TEN_US,
-                                    static_cast<double>(BASE_VERT_SERVO_MIN_TEN_US),
-                                    static_cast<double>(BASE_VERT_SERVO_MAX_TEN_US))};
+   // Write to serial device to move base servo to target position
+   std::array<uint8_t, COMMAND_LEN> command{};
+   command[0] = BASE_VERT_SERVO_NUM;
 
-   // Write to serial device to move base servo
-   const std::array<uint8_t, 2> command{BASE_VERT_SERVO_NUM, static_cast<uint8_t>(numTenUs)};
+   const float theta{static_cast<float>(rot.theta)};
+   const auto* ptr{reinterpret_cast<const uint8_t*>(&theta)};
+   for (size_t i = 0; i < sizeof(float); ++i)
+   {
+      command[i+1] = *(ptr+i);
+   }
+
    mySerial.writeToSerial(command.data(), command.size());
 }
 
